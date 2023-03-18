42abe7ea9eb63f8a8ecd55397656aa4e18eff4a7
fture <hyonjun311@gmail.com>
2023-03-19 03:14:20 +0900

    procfile 추가

--- app/main.py
+++ app/main.py
@@ -21,3 +21,7 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+@app.get("/", status_code=status.HTTP_200_OK)
+def root():
+    return {"Hello": "World"}
